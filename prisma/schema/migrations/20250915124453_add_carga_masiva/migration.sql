cd44a0a4e63204b8fa2c3c42c2d0701a4e116078
Guillermo Torres <torresrennerguilermo@gmai.com>
2025-09-25 16:18:07 -0300

    refacto tables

--- prisma/schema/migrations/20250915124453_add_carga_masiva/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."CargaMasivaArchivo" (
-    "id" SERIAL NOT NULL,
-    "NombreArchivo" TEXT NOT NULL,
-    "FechaIngreso" TIMESTAMP(3) NOT NULL,
-    "Estado" TEXT NOT NULL,
-
-    CONSTRAINT "CargaMasivaArchivo_pkey" PRIMARY KEY ("id")
-);
