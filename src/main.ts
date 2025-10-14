f31c083768ba3c974c2f6aedd6422bbfc1ef1dcb
root <root@desarrolloqa-sibne.tchile.com>
2025-10-14 22:33:01 +0000

    remove containers

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // Agrega esta línea para establecer el prefijo global
-  app.enableCors({
-    origin: true,
+   app.enableCors({
+    origin: ['*','http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
     credentials: true,
   });
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
-    .setTitle('backend Api Docs')
+    .setTitle('Backend Sibne Api Docs')
     .setDescription('Documentación de la API')
     .setVersion('1.0')
     .addBearerAuth()
